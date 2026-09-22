9dccde2bfe5af252a68d76bcf617aacc2c1f6ef4
Ullas MP <ullasmpu@gmail.com>
2026-09-22 12:11:07 +0530

    Created Models Objects and renderd the student details to Ui

--- Student_portal/student/views.py
+++ Student_portal/student/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-
+from .models import Student
 # Create your views here.
 def home(request):
     return render(request,'home.html')
 
 def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        roll = request.POST.get('roll_no')
+        mark = request.POST.get("marks")
+        sub = request.POST.get("subject")
+        Student.objects.create(name = name, roll_no = roll, marks = mark, subject = sub)
+
     return render(request,'create.html')
 
 def display(request):
-    return render(request,'display.html')
+    stu = Student.objects.all()
+    context = {"student":stu}
+    return render(request,'display.html',context)
